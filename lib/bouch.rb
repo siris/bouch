5b6bdd51d0e8d835112e1173beb286aba7856fbf
Shane R. Sofos <ssofos@gmail.com>
2017-12-15 20:02:18 -0800

    new style rubocop for literals and bouch class and method comment docs

--- lib/bouch.rb
+++ lib/bouch.rb
@@ -1,11 +1,14 @@
 require 'yaml'
 
+# Calculate and create simple financial budgets
+# by parsing a single YAML file as input
 class Bouch
   def initialize(file)
     @pouch = YAML.safe_load(IO.read(file))
     @quarters = Hash.new
   end
 
+  # Summarize and show all financial quarter budgets
   def show_quarters
     calc_quarters_raw(@pouch['Budget'])
     puts '---------------'
@@ -15,10 +18,12 @@ class Bouch
     puts '---------------'
   end
 
+  # Summarize and show the aggregate annual budget
   def show_annual_total
     puts format('%s %17.2f', 'Annual Total:', calc_quarters_raw_total.to_s)
   end
 
+  # Summarize and show the aggregate annual budget as a percentage of income
   def show_budget_percentage(income)
     puts format('%s %12.2f%s',
                 'Budget Percent:',
@@ -29,10 +34,12 @@ class Bouch
                 '%')
   end
 
+  # Summarize and show annual income
   def show_income(income)
     puts format('%s %16.2f', 'Annual Salary:', calc_salary(income['quantity'], income['frequency']))
   end
 
+  # Summarize and show all aggregate components of the quarterly and annual budgets
   def show_budget
     show_quarters
     show_annual_total
@@ -40,24 +47,29 @@ class Bouch
     show_budget_percentage(@pouch['Salary'])
   end
 
+  # Show the raw budget hash object parsed from the budget pouch YAML file
   def show_pouch
     puts @pouch
   end
 
   private
 
+  # Calculate the percentage of budget of a salary/income
   def calc_budget_percentage(total, salary)
     ((total.to_f / salary.to_f) * 100).round(2)
   end
 
+  # Calculate a quarterly repeating budget item
   def calc_repeating(item)
     (item.to_f * 3).round(2)
   end
 
+  # Calculate a week based reoccurring salary
   def calc_salary(amount, freq)
     (amount.to_f * (52 / freq)).round(2)
   end
 
+  # Calculate each financial quarters budget items, including repeating ones
   def calc_quarters_raw(budget)
     budget.each do |_q, items|
       @quarters[@quarters.length.to_s] = Array.new
@@ -74,6 +86,7 @@ class Bouch
     end
   end
 
+  # Calculate all financial quarter budget into an aggregate annual budget
   def calc_quarters_raw_total
     @quarters['0'].sum + @quarters['1'].sum + @quarters['2'].sum + @quarters['3'].sum
   end
